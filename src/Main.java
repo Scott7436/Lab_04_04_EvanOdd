e44170270f5615ca2ab4708c5bc08e158131a61d
Scott7436 <gogginse@mail.uc.edu>
2022-09-21 16:58:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int num = 50;
+
+        if((num % 2) == 0)
+        {
+            System.out.println("Your number is even");
+        }
+        else
+        {
+            System.out.println("Your number is odd");
+        }
+    }
+}
